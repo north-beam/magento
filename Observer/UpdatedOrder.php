148798e5b75c9027b5eabfb001e78bc3dc42de8d
Lukasz Byrdziak <lukasz@northbeam.io>
2022-12-29 16:03:58 +0100

    Code review changes

---
+++ Observer/UpdatedOrder.php
@@ -0,0 +1,74 @@
+<?php
+
+
+namespace Northbeam\OrderTracking\Observer;
+
+require_once __DIR__ . '/../lib/NbOrderObject.php';
+
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class UpdatedOrder implements ObserverInterface
+{
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Sales\Api\Data\OrderInterface $order,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->order = $order;  
+        $this->logger = $logger;
+    }
+
+    public function getClientID()
+    {
+        return $this->scopeConfig->getValue("northbeam/credentials/client_id");
+    }
+
+    public function getApiKey()
+    {
+        return $this->scopeConfig->getValue("northbeam/credentials/api_key");
+    }
+
+    public function execute(Observer $observer)
+    {
+        $api_key = $this->getApiKey();
+        $client_id = $this->getClientID();
+        $orderid = $observer->getOrderCreateModel()->getQuote()->getReservedOrderId();
+
+        if(!$orderid) {
+            return;
+        }
+
+        $order = $this->order->load($orderid);
+
+        $server_data = setup_northbeam_objects($order)->server_object;
+
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.northbeam.io/v1/orders',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $server_data,
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: $api_key",
+                "Content-Type: application/json",
+                "Data-Client-ID: $client_id"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        $this->logger->info($response);
+
+    }
+}
